393fbed903dfd3c60588e2b7d2eb4ea2f25d5b8d
Mary Harshitha <55683706+Mary-Harshitha-A@users.noreply.github.com>
2019-12-03 13:27:37 +0530

    AboutActivity
    Activity where the mioWeather description is displayed to the user

---
+++ AboutActivity.java
@@ -0,0 +1,35 @@
+package com.example.weather_app.AppManagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.weather_app.R;
+
+public class AboutActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        toolbar = findViewById(R.id.toolbarAbout);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
